113ee036b69af473e67dae5a8338d4c51bf3f44a
subodh.bisht <subodh.bisht1989@gmail.com>
2022-01-22 20:27:25 +0530

    OAuth setup via github

---
+++ src/test/java/com/bisht/bestreadweb/BestReadWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisht.bestreadweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestReadWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
