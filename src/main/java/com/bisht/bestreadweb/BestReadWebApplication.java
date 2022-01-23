076cfcb7c87637982b8d4a22cbb1361a1a453bb1
subodh.bisht <subodh.bisht1989@gmail.com>
2022-01-23 12:53:30 +0530

    Book page & controller added

--- src/main/java/com/bisht/bestreadweb/BestReadWebApplication.java
+++ src/main/java/com/bisht/bestreadweb/BestReadWebApplication.java
@@ -1,24 +1,39 @@
 package com.bisht.bestreadweb;
 
+import com.bisht.bestreadweb.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
+
 @SpringBootApplication
-@RestController
+@EnableCassandraRepositories
+//@RestController
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BestReadWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BestReadWebApplication.class, args);
 	}
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
+//	@RequestMapping("/user")
+//	public String user(@AuthenticationPrincipal OAuth2User principal) {
+//		System.out.println(principal);
+//		return principal.getAttribute("name");
+//	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties properties) {
+		Path bundle = properties.getSecureConnectBundle().toPath();
+		return cqlSessionBuilder -> cqlSessionBuilder.withCloudSecureConnectBundle(bundle);
 	}
 
 }
